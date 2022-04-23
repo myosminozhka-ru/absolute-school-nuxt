709acade09a60c4f7d1c7ba0e2b1f0ccd0e6d5f3
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-04-23 13:42:27 +0300

    cfg

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,10 +58,8 @@ export default {
     scss: ['./assets/scss/helpers/*.scss'],
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://viessmann-otoplenie.ru/local/api/site/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
