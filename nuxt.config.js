e9297b89313db2086d6dd9f1391e74049599e33c
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-04-24 14:52:15 +0300

    Проверка  на устройство

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,9 @@ export default {
     ['@nuxtjs/stylelint-module', {
       fix: true
     }],
-    '@nuxtjs/device',
+    ['@nuxtjs/device', {
+      defaultUserAgent: 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36'
+    }]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
