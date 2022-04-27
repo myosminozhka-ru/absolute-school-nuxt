2d85600390d30bfb35e14af61935a7c025415c48
Sergey Goncharovр <doimpos@gmail.com>
2022-04-27 20:48:03 +0500

    Правка тени

--- components/global/OsmButton.vue
+++ components/global/OsmButton.vue
@@ -207,7 +207,7 @@ export default {
     &:hover,
     &:active,
     &.isActive {
-      box-shadow: 0px vw(7) vw(24) rgba(230, 119, 31, 0.2);
+      box-shadow: 0px vw(7) vw(16) rgba(230, 119, 31, 0.2);
     }
   }
   &--order {
