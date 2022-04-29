7c211044e4432dbc34a171f9c6c985d118668ded
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-04-29 12:33:54 +0300

    lc

--- components/global/OsmHeader.vue
+++ components/global/OsmHeader.vue
@@ -161,6 +161,7 @@
       <span>Выход</span>
     </osm-button>
     <osm-burger :is-burger-opened.sync="isBurgerOpened" />
+    <v-tour name="myTour" :steps="steps"></v-tour>
   </header>
 </template>
 
@@ -170,6 +171,26 @@ export default {
   name: 'OsmHeader',
   data: () => ({
     isBurgerOpened: false,
+    steps: [
+      {
+        target: '#v-step-0',  // We're using document.querySelector() under the hood
+        header: {
+          title: 'Get Started',
+        },
+        content: `Discover <strong>Vue Tour</strong>!`
+      },
+      {
+        target: '.v-step-1',
+        content: 'An awesome plugin made with Vue.js!'
+      },
+      {
+        target: '[data-v-step="2"]',
+        content: 'Try it, you\'ll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.',
+        params: {
+          placement: 'top' // Any valid Popper.js placement. See https://popper.js.org/popper-documentation.html#Popper.placements
+        }
+      }
+    ]
   }),
   computed: {
     ...mapGetters('cart', {
