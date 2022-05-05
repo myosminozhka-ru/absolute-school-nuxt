53055daf19387f5c3bfcd957f2817fdeceee55d9
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-05-05 13:16:29 +0300

    prod config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    apps: [
-        {
-        name: 'absolute',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-        }
+    apps : [
+      {
+        name: "nuxt-dev",
+        script: "npm",
+        args: "run dev"
+      },
+      {
+        name: "nuxt-prod",
+        script: "npm",
+        args: "run start"
+      }
     ]
-}
+  }
