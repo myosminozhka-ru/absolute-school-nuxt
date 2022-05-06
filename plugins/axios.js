f21fc4ee9e987ce637f05ff84bf83b3f86700e09
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-05-06 12:30:13 +0300

    check cookies

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 const setCookie = require('set-cookie-parser');
 
-export default function ({ $axios, redirect, $nuxt }) {
+export default function ({ $axios, redirect, app }) {
     $axios.onResponse(response => {
-        console.log('PHPSESSID', $nuxt.$cookies.get('PHPSESSID'));
+        console.log('PHPSESSID', app.$cookies.get('PHPSESSID'));
     })
   }
