0445b41e6dc112df8355924c748f619e42689d4b
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-05-25 08:56:22 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,7 +98,7 @@ export default {
   },
 
   axios: {
-    baseURL: 'https://one.kutuzovv.ru/local/api/site/',
+    baseURL: process.env.NODE_ENV === 'dev' ? 'https://one.kutuzovv.ru/local/api/site/' : 'https://api.absolute-shop.ru/api/site/',
     withCredentials: true,
   },
 
